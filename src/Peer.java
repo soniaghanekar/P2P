8f1e5bfeeea808103b1f60b88ec1f470fe06e7f2
Priyanka <prishankaran1991@gmail.com>
2013-11-16 09:18:14 +0530

    Trying to send UploadPortNumber to server

--- src/Peer.java
+++ src/Peer.java
@@ -1,4 +1,8 @@
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Peer {
@@ -7,10 +11,52 @@ public class Peer {
 
     public static void main(String[] args) {
         try {
+            UploadServer uploadServer = new UploadServer();
+            uploadServer.start();
             Socket socket = new Socket("localhost", 7734);
+
+            BufferedWriter writer = getBufferedWriter(socket);
+
+            writer.flush();
+            writer.write(uploadServer.port);
+            writer.flush();
+            System.out.println("Sent Upload port= " + uploadServer.port);
+
+            socket.close();
         } catch (IOException e) {
             System.out.println("Server not found");
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
     }
+
+    private static BufferedWriter getBufferedWriter(Socket socket) throws IOException {
+        OutputStream outputStream = socket.getOutputStream();
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+        return new BufferedWriter(outputStreamWriter);
+    }
+
+    private static class UploadServer extends Thread {
+        ServerSocket uploadSocket;
+        int port;
+        UploadServer() {
+            try {
+                uploadSocket = new ServerSocket(0);
+                port = uploadSocket.getLocalPort();
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Socket accept = uploadSocket.accept();
+                } catch (IOException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+            }
+        }
+    }
 }
+
--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -21,6 +24,24 @@ public class Server {
         @Override
         public void run() {
             System.out.println("Hi.. This is a new connection for a peer");
+
+            try {
+                InputStream inputStream = socket.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+
+                long peerUploadPort =  Long.parseLong(reader.readLine());
+//                String s = reader.readLine();
+                System.out.println("Peer port = " + peerUploadPort);
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+            }
         }
     }
 
