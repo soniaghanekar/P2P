97c140f8a9f1ed176b5b794b1b05b6371c84f2d7
soniaghanekar <soniaghanekar@gmail.com>
2013-11-16 21:30:38 -0500

    Sending over list of available RFCs to server

--- src/Peer.java
+++ src/Peer.java
@@ -5,32 +5,71 @@ public class Peer {
 
     private String hostname;
     private UploadServer uploadServer;
+    PrintWriter writer;
+    Socket socket;
 
     public static void main(String[] args) {
+        Peer peer = new Peer();
         try {
-            Peer peer = new Peer();
-            peer.uploadServer.start();
-            Socket socket = new Socket("localhost", 7734);
 
-            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-
-            writer.println(peer.hostname);
-            writer.println(Integer.toString(peer.uploadServer.port));
+            peer.sendPeerInfo(peer);
             System.out.println("Sent Upload port= " + peer.uploadServer.port);
 
-            socket.close();
-        } catch (IOException e) {
-            System.out.println("Server not found");
+            peer.sendRFCList();
+
+        } finally {
+            try {
+                peer.socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void sendRFCList() {
+        File[] rfcs = new File("../RFC's").listFiles();
+
+        for (File rfc : rfcs) {
+            if (rfc.isFile())
+                sendRFCInfo(rfc);
+        }
+    }
+
+    private void sendRFCInfo(File rfc) {
+        String name = rfc.getName();
+        System.out.println(name);
+        try {
+
+            String title = new BufferedReader(new FileReader(rfc)).readLine().split(":")[1].trim();
+            System.out.println(title);
+            writer.println("ADD RFC " + name + " P2P-CI/1.0");
+            writer.println("Host: " + hostname);
+            writer.println("Port: " + uploadServer.port);
+            writer.println("Title: " + title);
+        } catch (Exception e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
+    }
+
+    private void sendPeerInfo(Peer peer) {
+        writer.println(peer.hostname);
+        writer.println(Integer.toString(peer.uploadServer.port));
+    }
+
+    private void connectToServer() throws IOException {
+        this.socket = new Socket("localhost", 7734);
+        this.writer = new PrintWriter(socket.getOutputStream(), true);
+
     }
 
     public Peer() {
         try {
             this.hostname = InetAddress.getLocalHost().getHostName();
             this.uploadServer = new UploadServer();
+            uploadServer.start();
+            connectToServer();
 
-        } catch (UnknownHostException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -38,6 +77,7 @@ public class Peer {
     private static class UploadServer extends Thread {
         ServerSocket uploadSocket;
         int port;
+
         UploadServer() {
             try {
                 uploadSocket = new ServerSocket(0);
--- src/Server.java
+++ src/Server.java
@@ -5,12 +5,14 @@ import java.util.*;
 public class Server {
 
     static List<Peer> peerList = new LinkedList<Peer>();
+    static List<RFCInfo> rfcInfoList = new LinkedList<RFCInfo>();
 
     public static void main(String[] args) throws IOException {
         ServerSocket listener = new ServerSocket(7734);
         while(true) {
             new RFCServer(listener.accept()).start();
             printPeers();
+            printRFCs();
         }
     }
 
@@ -34,6 +36,7 @@ public class Server {
 
             try {
                 registerPeer();
+                parseMessages();
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
@@ -45,6 +48,34 @@ public class Server {
             }
         }
 
+        private void parseMessages() {
+            try {
+                String line;
+                while((line = reader.readLine()) != null) {
+                    String method = line.split(" ")[0];
+                    System.out.println(method);
+                    if(method.equals("ADD"))
+                        parseAdd(line);
+//                    else if(method.equals("LOOKUP"))
+//                        parseLookup(line);
+//                    else
+//                        parseList(line);
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+
+        private void parseAdd(String line) throws IOException {
+            int number = Integer.parseInt(line.split(" ")[2]);
+            String host = reader.readLine().split(" ")[1];
+            reader.readLine();
+            String title = reader.readLine().split(" ")[1];
+            rfcInfoList.add(new RFCInfo(number, title, host));
+        }
+
         private void registerPeer() throws IOException {
             String peerHostName = reader.readLine();
             int peerUploadPort = Integer.parseInt(reader.readLine());
@@ -53,9 +84,9 @@ public class Server {
 
     }
     private static class Peer {
-
         String hostname;
         int port;
+
         private Peer(String hostname, int port) {
             this.hostname = hostname;
             this.port = port;
@@ -63,9 +94,29 @@ public class Server {
 
     }
 
+    private static class RFCInfo {
+        int number;
+        String title;
+        String hostname;
+
+        private RFCInfo(int number, String title, String hostname) {
+            this.number = number;
+            this.title = title;
+            this.hostname = hostname;
+        }
+    }
+
     private static void printPeers() {
+        System.out.println("Peers");
         for(Peer peer: peerList) {
             System.out.println("hostname = " + peer.hostname + " port = " + peer.port);
+        }
+    }
+
+    private static void printRFCs() {
+        System.out.println("RFCs");
+        for(RFCInfo rfc: rfcInfoList) {
+            System.out.println("number = " + rfc.number + " title = " + rfc.title + " hostname = " + rfc.hostname);
         }
     }
 
